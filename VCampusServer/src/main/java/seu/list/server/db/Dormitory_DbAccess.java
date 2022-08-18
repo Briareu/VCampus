e92137f5e60f73b07674dc61326708f78e2a7b6b
Isabell0z <98103492+Isabell0z@users.noreply.github.com>
2022-08-18 10:19:49 +0800

    Add files via upload

---
+++ VCampusServer/src/main/java/seu/list/server/db/Dormitory_DbAccess.java
@@ -0,0 +1,43 @@
+package main.java.seu.list.server.db;
+
+public class Dormitory_DbAccess {
+
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs=null;
+	
+	public Connection getConnection() {
+		try {
+			// 装载驱动
+			Class.forName("com.hxtt.sql.access.AccessDriver");
+			// 建立连接
+			String path = null;
+			try {
+				path = this.getClass().getResource("Dormitory.mdb").toURI().getPath().substring(1);
+			} catch (URISyntaxException e1) {
+				e1.printStackTrace();
+			} // 定位数据库文件的位置
+			String url = "jdbc:Access:///" + path; // Access 驱动
+
+			conn = DriverManager.getConnection(url);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+   			
+	public void closeConnection(Connection con,ResultSet rs,Statement s) {
+		try {
+			if (s!=null) {
+				s.close();
+			}
+			if (con!=null) {
+				con.close();	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
