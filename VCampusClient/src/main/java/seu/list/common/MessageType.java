677c9088502c2e6b02dfde39d7b9cfc0dc5e3fd7
Ranger <76956391+Briareu@users.noreply.github.com>
2022-08-27 19:58:06 +0800

    Update MessageType.java

--- VCampusClient/src/main/java/seu/list/common/MessageType.java
+++ VCampusClient/src/main/java/seu/list/common/MessageType.java
@@ -1,13 +1,16 @@
+//package VCampusServer.src.main.java.seu.list.common;
 package seu.list.common;
-//package seu.list.common;
 
 public final class MessageType {
-
-
+    public MessageType() {
+        // TODO Auto-generated constructor stub
+    }
 
     public static final String REQ_LOGIN="REQ_LOGIN";
     public static final String REQ_LOGOUT="REQ_LOGOUT";
     public static final String REQ_REGISTER="REQ_REGISTER";
+    public static final String REQ_USERDEL="REQ_USERDEL";
+    public static final String REQ_USERUPDATE="REQ_USERUPDATE";
     //Message-Type
     public static final String operFeedback = "OPER_FEEDBACK";
 
@@ -36,7 +39,7 @@ public final class MessageType {
 
     public static final String REQ_SEARCH_LESSON="REQ_SEARCH_LESSON";//数据库查询课程
     public static final String REQ_REMOVE_LESSON="REQ_REMOVE_LESSON";//老师加课
-    public static final String REQ_ADD_LESSON="REQ_ADD_LESSON"; //老师删课
+    public static final String REQ_ADD_LESSON="REQ_ADD_LESSON";//老师删课
     public static final String REQ_SHOW_ALL_LESSON="REQ_SHOW_ALL_LESSON";//显示全部课程
     public static final String REQ_STU_ADD_LESSON="REQ_STU_ADD_LESSON";//学生选课
     public static final String REQ_STU_REMOVE_LESSON="REQ_STU_REMOVE_LESSON";//学生退课
@@ -51,19 +54,21 @@ public final class MessageType {
     public static final String DormDelete="REMOVE_DORM";  //删除宿舍
     public static final String DormSearch="SEARCH_DORM";  //查找宿舍
     public static final String DormAdShow="SHOW_AD_DORM";  //管理员显示
+    public static final String DormModify ="MODIFY_DORM"; 
     public static final String DormStShow="SHOW_ST_DORM";  //学生显示
-    public static final String DormModify="MODIFY_DORM";  //管理员修改
     public static final String DormMaintain="MAINTAIN_DORM";  //维修设置
     public static final String DormExcange="REXCHANGE_DORM";  //调换设置
-    
+
     //Shop
     public static final String Goodsgetall="GETALL_GOODS";  
-    public static final String Goodsgetturnover="GET_TURNOVER"; //获得营收值
+    
     public static final String GoodsAdd="ADD_GOODS";  //管理员添加商品
+    public static final String Goodsgetturnover="GET_TURNOVER"; //获得营收值
     public static final String GoodsDelete="REMOVE_GOODS";  //管理员删除商品
     public static final String GoodsSearch_ID="SEARCH_GOODS_ID";  //ID查找商品
     public static final String GoodsSearch_Name="SEARCH_GOODS_NAME";  //名称查找商品
     public static final String Buy="BUY_GOODS";  //学生购买
     public static final String AddNumberofGoods="ADDNUMBER-GOODS";  //管理员进货
     public static final String ModifyGoodsPrice="MODIFYPRICE-GOODS"; //管理员改价格
+    
 }
