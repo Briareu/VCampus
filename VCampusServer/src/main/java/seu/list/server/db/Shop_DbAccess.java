05f6f041cbecc70266d145f21ca6f327eb415735
yushao12 <109010433+yushao12@users.noreply.github.com>
2022-09-01 00:54:48 +0800

    javadoc 更新

--- VCampusServer/src/main/java/seu/list/server/db/Shop_DbAccess.java
+++ VCampusServer/src/main/java/seu/list/server/db/Shop_DbAccess.java
@@ -8,16 +8,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**  
- * 
- *
+ * 类{@code Shop_DbAccess}是商店和数据库进行连接的基类，用的是ucanAccess jdbc驱动
  */
 public class Shop_DbAccess {
     static Connection con = null;
 	Statement stmt = null;
 	ResultSet rs=null;
-	/*public static void main(String[] args) {
-	   getConnection();
-	}*/
 	public static  Connection getConnection() {
 		try {
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
@@ -27,8 +23,6 @@ public class Shop_DbAccess {
 		}// 这个驱动的地址不要改
 		
 		try {
-			//con = DriverManager
-					//.getConnection("jdbc:ucanaccess://src/main/resources/db_vCampus.accdb");
 			con = DriverManager
 					.getConnection("jdbc:ucanaccess://src/main/resources/vCampus.accdb", "", "");
 		} catch (SQLException e) {
@@ -57,66 +51,6 @@ public class Shop_DbAccess {
 	}
 		}
 
-
-/*import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-
-public class Shop_DbAccess {
-	/**
-	 * Access数据库Connection
-	 
-	private Connection connection;
-
-	static {
-		try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");//加载ucanaccess驱动
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
-	public Connection getAccessConnection(String path) {
-		try {
-			//获取Access数据库连接(Connection)
-			this.connection = DriverManager.getConnection("jdbc:ucanaccess://" + path, user, pwd);
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-		return this.connection;
-	}
-
-	public static void main(String[] args) throws Exception {
-		/*Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");// 这个驱动的地址不要改
-		// Connection
-		// con=DriverManager.getConnection("jdbc:ucanaccess://C:\\1\\ACCESSdb.mdb","123456","123456");
-		Connection con = DriverManager
-				.getConnection("jdbc:ucanaccess://C:\\javaeclipse\\Vshop1.0\\Databaseshop.accdb");
-		Statement stmt = con.createStatement();
-	}
-
-		Shop_DbAccess access=new Shop_DbAccess();
-		Connection connection = access.getAccessConnection("C:\\javaeclipse\\Vshop1.0\\Databaseshop.accdb"", "", "");
-		access.select(connection);
-	}
-	/*public void select(Connection connection) throws Exception {
-		Statement statement = connection.createStatement();
-		ResultSet result = statement.executeQuery("select * from student");
-		while (result.next()) {
-			System.out.print(result.getString("id") + "\t");
-			System.out.print(result.getString("name") + "\t");
-			System.out.print(result.getString("address") + "\t");
-			System.out.print(result.getString("age") + "\t");
-			System.out.println();
-		}
-		statement.close();
-		connection.close();
-	}*/
-
-
-	
 	
 	
 	    
